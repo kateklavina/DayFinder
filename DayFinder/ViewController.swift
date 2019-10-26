b912997863e3610f7228c3e8207158d8b4e34db5
Dace Laizane <dace@Daces-MacBook-Air.local>
2019-10-26 12:10:54 +0300

    first commit

---
+++ DayFinder/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  DayFinder
+//
+//  Created by Dace Laizane on 25/10/2019.
+//  Copyright © 2019 Kate Klavina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var dayFinder: UILabel!
+    @IBOutlet weak var day: UILabel!
+    @IBOutlet weak var dayType: UITextField!
+    @IBOutlet weak var Month: UILabel!
+    @IBOutlet weak var monthType: UITextField!
+    @IBOutlet weak var year: UILabel!
+    @IBOutlet weak var yearType: UITextField!
+    @IBOutlet weak var dayOfWeek: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func dayFinderbutton(_ sender: UIButton) {
+    } //touch down
+}
+
