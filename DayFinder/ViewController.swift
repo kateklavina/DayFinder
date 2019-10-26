51e4097d2efe26a7766f6c8e7d5ac528b8fef886
Kate Klavina <kate.klavina97@gmail.com>
2019-10-26 12:58:06 +0300

    navigation

--- DayFinder/ViewController.swift
+++ DayFinder/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
- var str = " "
+ var str = ""
     @IBOutlet weak var dayFinder: UILabel!
     @IBOutlet weak var day: UILabel!
     @IBOutlet weak var dayType: UITextField!
@@ -20,9 +20,18 @@ class ViewController: UIViewController {
     @IBOutlet weak var dayOfWeek: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        statusBarStyle()
     }
-
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+    }
+    func statusBarStyle (){
+        let app = UINavigationBarAppearance()
+        app.backgroundColor = UIColor.red.withAlphaComponent(0.7)
+        app.largeTitleTextAttributes = [.foregroundColor: UIColor.white]
+        
+        self.navigationController?.navigationBar.scrollEdgeAppearance = app
+    }
 
     @IBAction func dayFinderbutton(_ sender: UIButton) {
     } //touch down
