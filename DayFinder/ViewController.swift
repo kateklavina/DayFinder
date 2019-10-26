82e15bf12241cabc756968e774edfb6d706b7631
Kate Klavina <kate.klavina97@gmail.com>
2019-10-26 15:59:36 +0300

    adiing funcionality to UI, NEED TO FIX LANDSCAPE UI

--- DayFinder/ViewController.swift
+++ DayFinder/ViewController.swift
@@ -11,13 +11,18 @@ import UIKit
 class ViewController: UIViewController {
  var str = ""
     @IBOutlet weak var dayFinder: UILabel!
-    @IBOutlet weak var day: UILabel!
+   
+    
     @IBOutlet weak var dayType: UITextField!
-    @IBOutlet weak var Month: UILabel!
+ 
+    
     @IBOutlet weak var monthType: UITextField!
-    @IBOutlet weak var year: UILabel!
+   
+    
     @IBOutlet weak var yearType: UITextField!
+    
     @IBOutlet weak var dayOfWeek: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         statusBarStyle()
@@ -31,9 +36,42 @@ class ViewController: UIViewController {
         app.largeTitleTextAttributes = [.foregroundColor: UIColor.white]
         
         self.navigationController?.navigationBar.scrollEdgeAppearance = app
+        // navigacija liela pa visu lauk.
+    }
+    @IBAction func dayFinderbutton(_ sender: UIButton) {
+        let calendar  = Calendar.current
+       
+    
+        
+        
+        var dateComponents = DateComponents()
+        
+        guard let day = dayType.text, let month = monthType.text, let year = yearType.text else {return}
+        dateComponents.day = Int(day)
+          dateComponents.month = Int(month)
+          dateComponents.year = Int(year)
+        
+    //    dateComponents.day = Int(dayType.text!)
+    //    dateComponents.month = Int(monthType.text!)
+    //    dateComponents.year = Int(yearType.text!)
+        
+    //    let date = calendar.date(from: dateComponents)
+        
+        guard let date  = calendar.date(from:dateComponents) else {return}
+        
+        let dateFormater = DateFormatter()
+        dateFormater.locale = Locale (identifier: "lv_LV")
+        dateFormater.dateFormat = "EEEE"
+        
+        let weekday = dateFormater.string(from: date)
+        let capitalizeWeekday = weekday.capitalized
+        
+        dayOfWeek.text = capitalizeWeekday
+    } //touch down
+    
+    
+    override func touchesBegan(_ touchesBegan: Set<UITouch>,with event: UIEvent?){
+        self.view.endEditing(true) //noņem keyborb
     }
 
-    @IBAction func dayFinderbutton(_ sender: UIButton) {
-    } //touch down
 }
-
